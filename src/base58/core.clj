bfed438d889c667309296f824750794a1fe17606
Diego Basch <dbasch@gmail.com>
2014-05-02 10:05:14 -0700

    updated docstrings

--- src/base58/core.clj
+++ src/base58/core.clj
@@ -6,8 +6,9 @@
 ;; The Bitcoin base58 alphabet
 (def code-string "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz")
 
-(defn int-to-base58 [num leading-zeros]
+(defn int-to-base58
   "Encodes an integer into a base58 string."
+  [num leading-zeros]
   (loop [acc []
          n num]
     (if (pos? n)
@@ -19,17 +20,19 @@
                   (repeat leading-zeros (first code-string))
                   acc)))))
 
-(defn encode [bytes]
+(defn encode
   "Encodes a byte array into a base58 string."
+  [bytes]
   (let [leading-zeros (->> bytes
-                       (take-while zero?)
-                       count)
+                           (take-while zero?)
+                           count)
         n (java.math.BigInteger. 1 (byte-array bytes))]
     (int-to-base58 n leading-zeros)))
 
-(defn decode [base58string]
+(defn decode
   "Decodes a base58-encoded string into a byte array.
    Returns nil if the string contains invalid base58 characters."
+  [base58string]
   (when (subset? (set base58string) (set code-string))
     (let [padding (->> base58string
                        (take-while #(= % (first code-string)))
@@ -47,14 +50,16 @@
                (concat padding)
                byte-array))))))
 
-(defn checksum [bytes]
+(defn checksum
   "Computes a Bitcoin-style double sha256 hash of a byte array, returns the first 4 bytes."
+  [bytes]
   (let [md (MessageDigest/getInstance "SHA-256")
         hash1 (.digest md bytes)]
     (take 4 (.digest md hash1))))
 
-(defn check [bytes]
+(defn check
   "True iff the last four bytes of a byte array are the checksum for the first part."
+  [bytes]
   (let [c1 (take-last 4 bytes)
         c2 (->> bytes
                 (drop-last 4)
@@ -63,8 +68,9 @@
                 checksum)]
     (= c1 c2)))
 
-(defn encode-check [hash header-byte]
+(defn encode-check
   "Converts a byte array into a bitcoin address (or key) with a header and a checksum."
+  [hash header-byte]
   (let [with-header (byte-array (cons header-byte hash))
         checked (->> with-header
                      checksum
@@ -72,15 +78,17 @@
                      (map byte))]
     (encode checked)))
 
-(defn get-payload [bytes]
+(defn get-payload
   "Drops the header and checksum. Call this method after decoding a key
    or an address, to obtain the 20-byte pubKey hash or the 32-byte key."
+  [bytes]
   (->> bytes
        (drop-last 4)
        next
        (map byte)
        byte-array))
 
-(defn valid? [s]
+(defn valid?
   "True iff a checksum-encoded string (key/address) has a valid checksum."
+  [s]
   (-> s decode check))
